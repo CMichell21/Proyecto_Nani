297ee7567c5c882e698b6b7f7717b5bb3446c2cc
CMichell21 <nancycarrillo200321@gmail.com>
2026-04-18 18:54:46 -0600

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,10 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: ['http://localhost:3000', 'https://localhost:443', process.env.FRONTEND_URL || '*',],
-    credentials: true,
-  });
+ app.enableCors({
+  origin: [
+    'http://localhost:3000',
+    'http://localhost:8081', // Expo web local
+    process.env.FRONTEND_URL || 'https://proyectonani123.nancarrillo20032114.workers.dev',
+  ],
+  credentials: true,
+});
 
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
